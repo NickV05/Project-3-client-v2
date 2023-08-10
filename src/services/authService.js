b43b69960a95eae2a496e5d305d60268902bd6f6
Nikita Valovyy <nickwolf046@gmail.com>
2023-08-10 16:40:22 -0400

    env

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,10 @@
 import axios from "axios"
-import { SERVER_URL } from "./SERVER_URL"
 
 
 export const get = (route) => {
     let token = localStorage.getItem('authToken')
 
-    return axios.get(SERVER_URL + route, {
+    return axios.get(process.env.SERVER_URL + route, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
@@ -13,7 +12,7 @@ export const get = (route) => {
 export const post = (route, body) => {
     let token = localStorage.getItem('authToken')
 
-    return axios.post(SERVER_URL + route, body, {
+    return axios.post(process.env.SERVER_URL + route, body, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
@@ -21,7 +20,7 @@ export const post = (route, body) => {
 export const put = (route, body) => {
     let token = localStorage.getItem('authToken')
 
-    return axios.put(SERVER_URL + route, body, {
+    return axios.put(process.env.SERVER_URL + route, body, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
@@ -29,7 +28,7 @@ export const put = (route, body) => {
 export const axiosDelete = (route) => {
     let token = localStorage.getItem('authToken')
 
-    return axios.delete(SERVER_URL + route, {
+    return axios.delete(process.env.SERVER_URL + route, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
