2a42155ab553ba372f740cf390999030e70e895d
Nikita Valovyy <nickwolf046@gmail.com>
2023-08-10 21:51:58 -0400

    data issue

--- src/components/ItemInProfile.jsx
+++ src/components/ItemInProfile.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-const ItemInProfile = ({ _id, name, image}) => {
+const ItemInProfile = ({ _id, name}) => {
 
   return (
     <div className = "flex flex-col justify-center align-middle">
