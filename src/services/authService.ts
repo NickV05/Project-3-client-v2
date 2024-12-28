ee6703847c06a0e9e46ac71159a359629bcd6602
Nikita Valovyy <nickwolf046@gmail.com>
2024-12-27 21:13:21 -0500

    update

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,31 +1,31 @@
 import axios from "axios"
 
-export const get = (route) => {
-    let token = localStorage.getItem('authToken')
+export const get = (route:string) => {
+    const token = localStorage.getItem('authToken')
 
     return axios.get(import.meta.env.VITE_SERVER_URL + route, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
 
-export const post = (route, body) => {
-    let token = localStorage.getItem('authToken')
+export const post = (route:string, body:unknown) => {
+    const token = localStorage.getItem('authToken')
 
     return axios.post(import.meta.env.VITE_SERVER_URL + route, body, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
 
-export const put = (route, body) => {
-    let token = localStorage.getItem('authToken')
+export const put = (route:string, body:unknown) => {
+    const token = localStorage.getItem('authToken')
 
     return axios.put(import.meta.env.VITE_SERVER_URL + route, body, {
         headers: { Authorization: `Bearer ${token}` }
     })
 }
 
-export const axiosDelete = (route) => {
-    let token = localStorage.getItem('authToken')
+export const axiosDelete = (route:string) => {
+    const token = localStorage.getItem('authToken')
 
     return axios.delete(import.meta.env.VITE_SERVER_URL + route, {
         headers: { Authorization: `Bearer ${token}` }
